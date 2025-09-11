b1e40d56147a58e690635d954e4c807050169fd5
7Fin7 <finley.leggett@gmail.com>
2025-09-11 18:09:28 +0100

    highestAndLowest

---
+++ Python/7kyu/highestAndLowest.py
@@ -0,0 +1,34 @@
+
+def high_and_low(numbers):
+    
+    # Split by space and convert list to int values
+    str_list = numbers.split(" ")
+    num_list = [int(x) for x in str_list]
+
+    # Default max and low to first number
+    highest = num_list[0]
+    lowest = num_list[0]
+
+    # Loop through second number onwards and compare to highest, lowest
+    for num in num_list[1:]:
+        if num > highest:
+            highest = num
+        if num < lowest:
+            lowest = num
+
+    return f"{highest} {lowest}"
+
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+print(high_and_low("1 2 3"))
+
+# In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+# Examples
+# high_and_low("1 2 3 4 5") # return "5 1"
+# high_and_low("1 2 -3 4 5") # return "5 -3"
+# high_and_low("1 9 3 4 -5") # return "9 -5"
+
+# Notes
+# All numbers are valid Int32, no need to validate them.
+# There will always be at least one number in the input string.
+# Output string must be two numbers separated by a single space, and highest number is first.
