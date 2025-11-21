ec5e0180f6cfd57bf0236585027d538b320d0cef
7Fin7 <finley.leggett@gmail.com>
2025-11-21 16:25:54 +0000

    countingDuplicates

---
+++ Python/6kyu/countingDuplicates.py
@@ -0,0 +1,32 @@
+
+def duplicate_count(text):
+    
+    char_count = {}
+    dup_count = 0
+
+    for char in text.lower():
+        char_count[char] = char_count.get(char, 0) + 1
+
+    for count in char_count.values():
+        if count > 1:
+            dup_count += 1
+
+    return dup_count
+
+print(duplicate_count("abcde"))
+print(duplicate_count("aabbccdd"))
+
+# Count the number of Duplicates
+# Write a function that will return the count of distinct case-insensitive alphabetic 
+# characters and numeric digits that occur more than once in the input string. The 
+# input string can be assumed to contain only alphabets (both uppercase and lowercase) 
+# and numeric digits.
+
+# Example
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+# "indivisibility" -> 1 # 'i' occurs six times
+# "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+# "aA11" -> 2 # 'a' and '1'
+# "ABBA" -> 2 # 'A' and 'B' each occur twice
